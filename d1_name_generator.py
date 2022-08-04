8d0b7248493af95861940cf9a6eec07fbbdab423
Termos <evgeny.zakharov.liga@gmail.com>
2022-08-05 00:38:10 +0300

    add generator

---
+++ d1_name_generator.py
@@ -0,0 +1,5 @@
+#comment
+print("Welcome to the Stripper Nickname Generator.")
+street_name = input("Which street did you grew up in?\n")
+first_pet_name = input("What is your first pet's name?\n")
+print("Your Stripper Nickname is " + first_pet_name + " " + street_name)
